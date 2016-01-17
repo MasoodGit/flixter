70a992053c2e257e0ef5fd84e250fbfd2a4bf7ce
masoodgit <mohamed.alam78@gmail.com>
2016-01-17 15:55:03 +0000

    adding devise plugin

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Flixter::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Setting default URL Options for devise
+  config.action_mailer.default_url_options = { host: 'localhost:3030' }
 end
